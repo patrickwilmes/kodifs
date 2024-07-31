4ae1120038bc43d8db1a1d3721ce4802f8430020
Patrick Wilmes <p.wilmes89@gmail.com>
2024-07-31 21:58:57 +0200

    Basic Ktor Setup
    Hooking up the required ktor libraries including kotlin coroutines for
    both the main and data nodes. The client will use different setup.

--- data-node/build.gradle.kts
+++ data-node/build.gradle.kts
@@ -1,15 +1,32 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    id(Plugins.KtorPlugin) version Plugins.KtorPluginVersion
+    id(Plugins.KotlinxSerialization) version Plugins.KotlinxSerializationVersion
 }
 
 group = "com.bitlake"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(Dependencies.KtorContentNegotiation)
+    implementation(Dependencies.KtorCore)
+    implementation(Dependencies.KtorKotlinxJson)
+    implementation(Dependencies.KtorCors)
+    implementation(Dependencies.KtorNetty)
+    implementation(Dependencies.LogBack)
+    implementation(Dependencies.KtorServerConfig)
+    implementation(Dependencies.Coroutines)
     testImplementation(kotlin("test"))
 }
 
---
+++ main-node/src/main/kotlin/com/bitlake/main/App.kt
@@ -0,0 +1,11 @@
+/*
+* Copyright (c) 2024, Patrick Wilmes <p.wilmes89@gmail.com>
+* All rights reserved.
+*
+* SPDX-License-Identifier: BSD-2-Clause
+*/
+package com.bitlake.main
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
